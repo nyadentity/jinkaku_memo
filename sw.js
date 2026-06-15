1cc8d2d01a3583906911e40f5ac703e7ca94fbae
nyadentity <sbmskn20471055@gmail.com>
2026-06-15 23:00:31 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "jinkaku-sns-v96";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.2/babel.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      return res;
+    }).catch(() => cached))
+  );
+});
